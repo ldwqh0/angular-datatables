f0f3f1dba7481fb2182619fa6ee58dd6e982b45f
xz <ldwqh0@outlook.com>
2017-08-21 18:06:51 +0800

    修正了分页模板再ie下会发生链接跳转的问题

--- src/lib/module.js
+++ src/lib/module.js
@@ -1,3 +1,13 @@
 import angular from 'angular'
+
 const module = angular.module('angular.datatables', [])
+module.run(['$templateCache', function ($templateCache) {
+  $templateCache.put('uib/template/pagination/pagination2.html',
+    `<li role="menuitem" ng-if="::boundaryLinks" ng-class="{disabled: noPrevious()||ngDisabled}" class="pagination-first"><a href="javascript:void(0)" ng-click="selectPage(1, $event)" ng-disabled="noPrevious()||ngDisabled" uib-tabindex-toggle>{{::getText('first')}}</a></li>
+    <li role="menuitem" ng-if="::directionLinks" ng-class="{disabled: noPrevious()||ngDisabled}" class="pagination-prev"><a href="javascript:void(0)" ng-click="selectPage(page - 1, $event)" ng-disabled="noPrevious()||ngDisabled" uib-tabindex-toggle>{{::getText('previous')}}</a></li>
+    <li role="menuitem" ng-repeat="page in pages track by $index" ng-class="{active: page.active,disabled: ngDisabled&&!page.active}" class="pagination-page"><a href="javascript:void(0)"f ng-click="selectPage(page.number, $event)" ng-disabled="ngDisabled&&!page.active" uib-tabindex-toggle>{{page.text}}</a></li>
+    <li role="menuitem" ng-if="::directionLinks" ng-class="{disabled: noNext()||ngDisabled}" class="pagination-next"><a href="javascript:void(0)" ng-click="selectPage(page + 1, $event)" ng-disabled="noNext()||ngDisabled" uib-tabindex-toggle>{{::getText('next')}}</a></li>
+    <li role="menuitem" ng-if="::boundaryLinks" ng-class="{disabled: noNext()||ngDisabled}" class="pagination-last"><a href="javascript:void(0)" ng-click="selectPage(totalPages, $event)" ng-disabled="noNext()||ngDisabled" uib-tabindex-toggle>{{::getText('last')}}</a></li>`
+  )
+}])
 export default module
